29a3d497c0b7ca6e45ff74d7104f08dfd0cca9fc
Gauravkumar Sonawane <gauravsonawane764@gmail.com>
2024-01-09 13:20:08 +0530

    added prac

---
+++ prac_que.js
@@ -0,0 +1,7 @@
+// print 1 to 20 numbers which are divisible 3 & 5
+
+for(let i = 1 ; i<=20 ; i++){
+    if(i%3 === 0 && i%5 === 0){
+        console.log(i);
+    }
+}
--- practice_que.js
+++
@@ -1,6 +0,0 @@
-
-for(let i = 1 ; i<=20 ; i++){
-    if(i%3 === 0 || i%5 === 0){
-        console.log(i);
-    }
-}
