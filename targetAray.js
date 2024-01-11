92b58417f7cf5d04aaf8f3d96df6f617302d2070
Gauravkumar Sonawane <gauravsonawane764@gmail.com>
2024-01-11 12:28:46 +0530

    update_targetArray

--- targetAray.js
+++ targetAray.js
@@ -2,13 +2,15 @@
 Input: nums = [2,7,11,15], target = 9
 Output: [0,1] */
 
-let nums = [2 , 7 , 11 , 15];
-let target = 9 ;
+let nums = [2,7,5,11,15,1];
+let target = 6 ;
 const output = (arr , tar) =>{
     let arr2 = [] ;
     for(let i = 0 ; i < arr.length ; i++){
-        if(arr[i] + arr[i+1] == tar){
-            arr2.push(i , (i+1));
+        for(let j = i+1 ; j<arr.length ; j++){
+            if(arr[i] + arr[j] == tar){
+                arr2.push(i , j);
+            }
         }
     }
     return arr2 ;
