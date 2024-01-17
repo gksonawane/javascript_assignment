758f067efaca8a7add52b58d92a1a31d5fb0a19b
Gauravkumar Sonawane <gauravsonawane764@gmail.com>
2024-01-17 12:40:37 +0530

    add_ageInDays

---
+++ jsAssign17/ageDays.js
@@ -0,0 +1,13 @@
+// Create a function that takes the age in years and returns the age in days.
+
+const ageIndays = (yrs) => {
+    let oneYear = 365 ;
+    
+    if(yrs === 0){
+        return yrs ;
+    }
+    
+    return yrs * oneYear ;
+}
+
+console.log("Your age in days are :",ageIndays(2.5));
